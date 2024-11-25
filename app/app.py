6ecf21c69f1e229ee0d53428eb9cf502c25591f9
wkarto <kwomenker14@gmail.com>
2024-11-25 18:59:03 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -7,7 +7,7 @@ import palmerpenguins
 
 df = palmerpenguins.load_penguins()
 
-ui.page_opts(title="Penguins dashboard", fillable=True)
+ui.page_opts(title="Penguins analysis dashboard", fillable=True)
 
 
 with ui.sidebar(title="Filter controls"):
